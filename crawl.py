e94c90d6aaee988de7f182640eb1f2d58c71d305
mike <mpobrien005@gmail.com>
2009-10-25 20:24:34 -0400

    playing around with lucene again

--- crawl.py
+++ crawl.py
@@ -1,6 +1,13 @@
 import urllib, urllib2
 from BeautifulSoup import BeautifulSoup
 
+class MissingData(Exception):#{{{
+    def __init__(self, message):
+        Exception.__init__(self)
+        self.message = message
+    def __repr__(self):
+        return self.message#}}}
+
 def gethtml(word):#{{{
     url = "http://www.conjugation.org/cgi-bin/conj.php"
     "word"
@@ -25,19 +32,50 @@ def gethtml(word):#{{{
     response = urllib2.urlopen(req)
     return response.read()#}}}
 
-def conjugate(html):
+def conjugate(html):#{{{
     soup = BeautifulSoup( html )
     tags = soup.findAll('strong')
     alltenses = ['Present Indicative:', 'Imperfect:', 'Preterite:', 'Future:', 'Conditional:', 'Imperative:', 'Present Subjunctive:', 'Imperfect Subjunctive:', 'Gerund:','Past Participle:']
     resultsDict = {}
     for t in tags:
         if t.string and t.string in alltenses:
-            limit = 6
+            limit = 7
             if t.string in alltenses[-2:]: limit = 2
+            if t.string == alltenses[5]: limit = 6
             key = t.string
             values = t.findAllNext(text=True, limit = limit)[1:]
-            values = [v.encode('windows-1252').strip() for v in values]
+            values = [v.strip() for v in values]
             resultsDict[key] = values
-    return resultsDict
+    return resultsDict#}}}
 
+def fortext(conj, infinitive):#{{{
+    alltenses = ['Present Indicative:', 'Imperfect:', 'Preterite:', 'Future:', 'Conditional:', 'Imperative:', 'Present Subjunctive:', 'Imperfect Subjunctive:', 'Gerund:','Past Participle:']
+    output_list = [infinitive]
+    for t in alltenses:
+        results = conj[t]
+        if t in (alltenses[-1], alltenses[-2]): 
+            if len(results) != 1: raise MissingData(t + ": " + str(len(results)))
+        else: 
+            if t == alltenses[5]:
+                if len(results) != 5:  
+                    print t, len(results)
+                    raise MissingData(t + ": " + str(len(results)))
+            elif len(results) != 6:  
+                print t, len(results)
+                raise MissingData(t + ": " + str(len(results)))
+        output_list += results
+    return ' '.join(output_list)#}}}
 
+def doit(file, outfile):
+    for line in file:
+        try:
+            theword = line.strip()
+            outline = fortext(conjugate(gethtml(theword)), theword).encode('utf-8')
+            outfile.write( outline + '\n' )
+        except:
+            print line, "skipping"
+
+if __name__ == '__main__':
+    infile = open('verbs.txt','r')
+    outfile = open('output.txt','w')
+    doit(infile, outfile)
